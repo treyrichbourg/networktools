9cddb51c64fc03f89a352906ce3ac8af49a9d907
Trey <trichbourg@1099r227bnp12.ITTN.NAVY>
2022-07-31 00:08:11 -0500

    added set_creds

--- Nornir/Tools/get_creds.py
+++ Nornir/Tools/get_creds.py
@@ -1,23 +1,16 @@
 #! /usr/bin/python3
+import getpass
+
 """
 Convert user input into default credentials for Nornir.
 """
 
-import getpass
-
 
 def get_creds():
     username = input("Enter Username: ")
-    password = getpass.getpass("Enter Password:")
+    password = getpass.getpass("Enter Password: ")
     secret = getpass.getpass("Enter Secret: ")
     return username, password, secret
-
-
-def set_credentials(task):
-    username, password, secret = get_creds()
-    task.inventory.defaults.username = username
-    task.inventory.defaults.password = password
-    task.inventory.defaults.connection_options["netmiko"].extras["secret"] = secret
 
 
 def get_user():
@@ -26,10 +19,17 @@ def get_user():
 
 
 def get_password():
-    password = getpass.getpass("Enter Password:")
+    password = getpass.getpass("Enter Password: ")
     return password
 
 
 def get_secret():
     secret = getpass.getpass("Enter Secret: ")
     return secret
+
+
+def set_creds(task):
+    username, password, secret = get_creds()
+    task.inventory.defaults.username = username
+    task.inventory.defaults.password = password
+    task.inventory.defaults.connection_options["netmiko"].extras["secret"] = secret
